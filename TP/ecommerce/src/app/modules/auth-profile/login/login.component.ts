7f2bcd2d69b349e07229ca915cf5b8939c24a317
BeluTomas <belutomas2003@gmail.com>
2024-05-12 22:19:35 -0300

    Registro de usuarios frontend

--- TP/ecommerce/src/app/modules/auth-profile/login/login.component.ts
+++ TP/ecommerce/src/app/modules/auth-profile/login/login.component.ts
@@ -17,7 +17,10 @@ export class LoginComponent implements OnInit{
   ) {}
 
   ngOnInit(): void{
-    console.log(this.authService.user);
+    // console.log(this.authService.user);
+    if(this.authService.user){
+      this.router.navigate(["/"]);
+    }
   }
   
   login(){
